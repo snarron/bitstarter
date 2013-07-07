aa674cf7d63c7cfe28465b7069e206b62c0785bf
Shohei Narron <shohei.narron@gmail.com>
2013-07-07 09:22:43 -0700

    Cleaned bitstarter

---
+++ bitstarter/web.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var fs = require('fs');
+
+var app = express.createServer(express.logger());
+var readFile = fs.readFileSync("index.html", "utf-8");
+var buffer = new Buffer(readFile, "utf-8");
+
+app.get('/', function(request, response) {
+  response.send(buffer.toString("utf-8"));
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
