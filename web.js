04c3f06beaf2f1e443ecd587f33604ea0f359259
snarron <shohei.narron@gmail.com>
2013-07-23 03:13:07 +0000

    Commented out authentication code. It worked, though.

--- web.js
+++ web.js
@@ -5,9 +5,11 @@ var app = express.createServer(express.logger());
 var readFile = fs.readFileSync("index.html", "utf-8");
 var buffer = new Buffer(readFile, "utf-8");
 
+/*
 app.use(express.basicAuth(function(user, pass){
   return 'sho' == user & 'narron' == pass;
 }));
+*/
 
 app.get('/', function(request, response) {
   response.send(buffer.toString("utf-8"));
